fbebcc3dec731df73de464282f73e70f7f8a5727
VincentVergioUMN <vincent.vergio@student.umn.ac.id>
2025-11-05 11:18:43 +0700

    Class Car implements Vehicle

---
+++ src/week09/vincent/id/ac/umn/maintutor.java
@@ -0,0 +1,15 @@
+package week09.vincent.id.ac.umn;
+
+public class maintutor{
+
+	public static void main(String[] args){
+
+		Car myCar = new Car("Honda Civic");
+
+		myCar.showVehicleInfo();
+
+		myCar.start();
+
+		myCar.stop();
+	}
+}
