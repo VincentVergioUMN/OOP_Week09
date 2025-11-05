470fa0914cbea64ba6b23683b815f8addbfaa73c
VincentVergioUMN <vincent.vergio@student.umn.ac.id>
2025-11-05 11:00:17 +0700

    Interface Animal

---
+++ src/week09/vincent/id/ac/umn/Animal.java
@@ -0,0 +1,6 @@
+package week09.vincent.id.ac.umn;
+
+public interface Animal {
+	public void eat();
+	public void travel();
+}
---
+++ src/week09/vincent/id/ac/umn/Mammal.java
@@ -0,0 +1,20 @@
+package week09.vincent.id.ac.umn;
+
+public class Mammal implements Animal{
+	
+	public Mammal() {}
+	
+	public void eat() {
+		System.out.println("Mammal eats");
+	}
+	
+	public void travel() {
+		System.out.println("Mammal travel");
+	}
+	
+	public static void main(String[] args) {
+		Mammal m = new Mammal();
+		m.eat();
+		m.travel();
+	};
+}
